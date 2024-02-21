5ac6aabface6a387171e1576bed6d74697c00c08
Krawk013 <109297452+Krawk013@users.noreply.github.com>
2024-02-21 09:46:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-// index.js
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const fs = require('fs');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+
+let messages = [];
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -14,14 +16,28 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   console.log('Novo usuário conectado');
 
+  socket.emit('chat history', messages);
+
   socket.on('chat message', (msg) => {
+    messages.push(msg);
     io.emit('chat message', msg);
+    saveMessages();
   });
 
   socket.on('disconnect', () => {
     console.log('Usuário desconectado');
   });
 });
+
+function saveMessages() {
+  fs.writeFile('messages.json', JSON.stringify(messages), (err) => {
+    if (err) {
+      console.error('Error saving messages:', err);
+    } else {
+      console.log('Messages saved successfully.');
+    }
+  });
+}
 
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
